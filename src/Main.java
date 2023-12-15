a83fd9cbae2892f44772dbc3522c9a8f92c7a6f8
BT_2N2_33 <sample@bths.edu>
2023-12-15 13:56:48 -0500

    second partner adding code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String[] args) {
         System.out.println("hello people");
-        System.out.println("Owen Hong moment");
+        System.out.println("skibidi rizz");
         System.out.println("yes sir");
     }
 }
