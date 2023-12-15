7659052a7203887e3c305feb3a4df9a4cb920c6b
BT_2N2_32 <sample@bths.edu>
2023-12-15 13:56:46 -0500

    changed line

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("hello people");
         System.out.println("Owen Hong moment");
-        System.out.println("yes sir");
+        System.out.println("nuh uh");
     }
 }
